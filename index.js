3950d4d89686a7e1730c236dd91dd99ab5d24422
joowon <saven7788@gmail.com>
2017-07-06 11:57:38 +0900

    imagemagick test

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const im = require('imagemagick');
+
+// 시간기준은 잘 안된다
 exports.helloWorld = (req, res) => {
-    res.send('helloWorld!');
+    im.readMetadata('./dog.png', function(err, metadata){
+        if (err) throw err;
+        res.send('Shot at '+metadata.exif.dateTimeOriginal);
+    });
+
 };
+
+
+// function disk가 있따 임시공간 운이 좋음 꺼낼 수도 있다. cache같은 것
+// buffer
